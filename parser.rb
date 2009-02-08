8645d9a62a2c4db1a86dd05642bb3b53f9e5d433
soo <soo@bigfoot.(none)>
2009-02-07 21:11:35 -0600

    Time to rip up the parser

--- parser.rb
+++ parser.rb
@@ -1,23 +1,85 @@
-#! /usr/bin/ruby -w
+#!/usr/local/bin/ruby -w
+
+def this_thing
+	final = Hash.new()
+
+	class_name = @attributes.keys[@attributes.length-1]
+	@attributes[class_name].each do |key, val|
+		final[key] = @attributes
+	end
+
+	final.each do |key, val|
+		puts "#{key} => #{val}"
+		puts '-'*50
+	end
+end
 
 arff_file = File.open('data/adult.arff', 'r')
 relation_name = nil
-attributes = Hash.new()
+@attributes = Hash.new()
 
-while line = arff_file.gets
+i = 0
+while line = arff_file.gets.chomp!
 
 	if line[0] == '%' then 
+
 		# ignore since its a comment
+		
 	elsif line =~ /@RELATION/ then
+
 		relation_name = line[10..-1]
+
 	elsif line =~ /@ATTRIBUTE/ then
-		line = line.split(' ', 2)
-		puts line
-		attributes[line[1]] = line[2]
+
+		line = line[11..-1] # chop off the @ATTRIBUTE tag
+		line = line.split(/\t+| /, 2)
+
+		attr_name = line[0]
+		attr_val = line[1]
+
+		if attr_val[0] == '{' then
+			# split up the list of @attributes
+			
+			attr_val[attr_val.index('{')] = ''
+			attr_val[attr_val.index('}')] = ''
+
+			attr_val = attr_val.split(',')
+
+			# turns the values into a hash so we can count the occurances
+			values = Hash.new()
+			attr_val.each do |_attr|
+				values[_attr] = 0
+			end
+
+			attr_val = values
+		else
+			attr_val = 0
+		end
+
+		@attributes[attr_name] = attr_val
+
+	elsif line =~ /@DATA/ then
+
+		# do nothing
+		this_thing()
+		
+	elsif line != "" then
+
+		line = line.split(',')
+		clas = line[line.length-1]
+
+		@attributes[clas] = line
 	end
 
-
+	# debug block
+	if i > 25 then
+		break
+	end
+	i += 1
 end
 
-puts relation_name
-puts attributes.to_a
+@attributes.each do |key, val|
+	#puts "#{key} => #{val}"
+end
+
+
