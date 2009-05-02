8f3082446d7993cb72459c4395a03aced9538833
Jason Soo <wwwjscom@gmail.com>
2009-05-02 06:25:37 -0500

    release candidate

--- p4/logic.rb
+++ p4/logic.rb
@@ -1,8 +1,10 @@
 class Logic
 
-  MIN_FREQ = 1200
+  MIN_FREQ = 1
 
-  def initialize(transactions, products, freq = 4)
+  attr_accessor :total_rules, :top_3_rules
+
+  def initialize(transactions, products, c, s)
     @p = products
     @t = transactions
     @total_p = @p.file[0].size
@@ -10,6 +12,10 @@ class Logic
     @infreq_sets = Array.new
     @products = Array.new
     setup_products_array
+    @total_rules = 0
+    @top_3_rules = [ [0,0], [0,0], [0,0] ]
+    @min_c = c
+    @min_s = s
   end
 
   # P(A intersect B)
@@ -60,9 +66,17 @@ class Logic
           candidate_set = (item_set.to_a | item_set_2.to_a).sort
           freq = calc_freq(candidate_set)
 
-          if candidate_set.size == k and freq > MIN_FREQ and !@infreq_sets.include?(candidate_set) and !new_set.include?(candidate_set)
+          # calculate confidence
+          last = candidate_set.last
+          c = confidence(candidate_set[0..-2], last)
+          # calculate support
+          s = support(candidate_set[0..-2], last)
+
+          #if candidate_set.size == k and freq > MIN_FREQ and !@infreq_sets.include?(candidate_set) and !new_set.include?(candidate_set)
+          if candidate_set.size == k and c > @min_c and s > @min_s and !@infreq_sets.include?(candidate_set) and !new_set.include?(candidate_set)
             new_set << candidate_set
-          elsif freq <= MIN_FREQ and !@infreq_sets.include?(candidate_set)
+          #elsif freq <= MIN_FREQ and !@infreq_sets.include?(candidate_set)
+          elsif (c <= @min_c or s <= @min_s) and !@infreq_sets.include?(candidate_set)
             @infreq_sets << candidate_set
           end
 
@@ -71,10 +85,10 @@ class Logic
     end
 
     # DEBUG LOOP
-    @infreq_sets.each do |x|
-      print "[#{x}], "
-    end
-      puts
+#    @infreq_sets.each do |x|
+#      print "[#{x}], "
+#    end
+#      puts
 
     return new_set
   end
@@ -137,6 +151,36 @@ class Logic
     end
 
     return set_a
+  end
+
+  def print_rules(set)
+    set.each do |entry|
+      last = entry.last
+      entry[0..-2].each {|x| print x, ',' }
+      print "=> ", last
+      puts
+    end
+  end
+
+  def count_rules(set)
+    @total_rules += set.size
+  end
+
+  def determine_top_rules(set)
+    set.each do |item|
+      last = item.last
+      c = confidence(item[0..-2], last)
+      @top_3_rules.each do |rule|
+        if rule[0] < c
+          rule[0] = c
+          rule_output = ""
+          item[0..-2].each {|x| rule_output << "#{x}, "}
+          rule_output << "=> #{last}"
+          rule[1] = rule_output
+          break
+        end
+      end
+    end
   end
 
 end
--- p4/main.rb
+++ p4/main.rb
@@ -3,6 +3,12 @@
 require 'parser'
 require 'logic'
 
+c = ARGV[0].to_f # Confidence
+s = ARGV[1].to_f # Support
+
+puts c
+puts s
+
 @t = Parser.new('test_transactions.csv')
 @p = Parser.new('test_products')
 
@@ -11,27 +17,40 @@ require 'logic'
 
 @p.read_file
 @t.read_file
-@l = Logic.new(@t, @p)
+@l = Logic.new(@t, @p, c, s)
 
 def _debug(k)
   for i in (1..k)
-    puts "Item \t\t Freq. \t\t Support \t\t Confidence"
-    puts "-"*25
+    #puts "Item \t\t Freq. \t\t Support \t\t Confidence"
+    #puts "-"*25
     @set = @l.find_c(i, @set)
     @set.each do |item|
       if i == 1 then
-        puts "#{item} \t\t #{@l.calc_freq(item)}"
+        #puts "#{item} \t\t #{@l.calc_freq(item)}"
       else
-        print "#{item} \t\t #{@l.calc_freq(item)} \t\t"
-        last = item.last
-        print @l.support(item[0..-2], last)
-        print "\t\t", @l.confidence(item[0..-2], last)
-        puts
+        #print "#{item} \t\t #{@l.calc_freq(item)} \t\t"
+        #last = item.last
+        #print @l.support(item[0..-2], last)
+        #print "\t\t", @l.confidence(item[0..-2], last)
+        #puts
       end
     end
 
-    puts "\n\n"
+    #puts "Itemsets @ #{i}: #{@set.size}"
+    print @set.size, ','
+
+    if i > 1
+   #   @l.print_rules(@set)
+   #   puts "\n\n"
+      @l.count_rules(@set)
+      @l.determine_top_rules(@set)
+    end
+
   end
+
+  print "\n\n"
+  puts "Total rules: #{@l.total_rules}"
+  @l.top_3_rules.each {|c, r| print "Confidence: #{c} \t\t\t Rule: #{r}\n"}
 end
 
-_debug(2)
+_debug(4)
